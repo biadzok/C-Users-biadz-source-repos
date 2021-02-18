36eea66d713da30f05866b4ab037b19b423a9c94
Marin BAILHE <marin.bailhe@epita.fr>
2021-02-18 14:07:43 +0000

    cours 2 pause

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -15,10 +15,27 @@ namespace WebApplication1.Controllers
         public static List<Games> GetGames()
         {
             List<Games> games = new List<Games>();
-            games.Add(new Games(){ Id - 1, Name - "Game 1", Price - 10 });
-            games.Add(new Games(){ Id - 1, Name - "Game 1", Price - 10 }); 
-            games.Add(new Games(){ Id - 1, Name - "Game 1", Price - 10 });
+            games.Add(new Games(){ Id = 1, Name = "Game 1", Price = 25 });
+            games.Add(new Games(){ Id = 2, Name = "Game 2", Price = 10 }); 
+            games.Add(new Games(){ Id = 3, Name = "Game 3", Price = 45 });
             return games;
+        }
+
+        [HttpGet]
+
+        public IEnumerable<Games> GetGames_List()
+        {
+            return GetGames();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Games> GetGames_ById(int id)
+        {
+            var games = GetGames().SingleOrDefault(x => x.Id == id);
+            if (games != null)
+                return games;
+            else
+                return NotFound();
         }
         
     }
